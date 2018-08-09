151435c7823ab8b1e2a4d15c246c0a34ac821466
oleg <olegolegoleg525@gmail.com>
2018-08-09 13:30:51 +0300

    No changes

---
+++ main.js
@@ -0,0 +1,11 @@
+
+ var txtportfolio = document.getElementById('txtportfolio').style;
+
+
+if (body.scrollTop>300){ 
+        txtportfolio.opacity="0";
+        } 
+    else { 
+           txtportfolio.opacity="1";
+           
+           }
